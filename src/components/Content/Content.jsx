0af2f82ee090d4cfe88aafd1b2030827bd73b2c7
Serhii Zhitnik <135828574+allhitsugaya@users.noreply.github.com>
2025-03-23 13:14:46 +0200

    adding router.config, debugging content, principe of index in route

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import './Content.css'
+import {topics} from "../route/router.config.js";
 
 const TopicsListPage = () => {
-    const topics = [
-        { id: 1, title: "Змінні та типи данних", path: '/variables'  },
-        { id: 2, title: "Оператори", path: '/operators'  },
-        { id: 3, title: "Цикли",path: '/cycles'  },
-        { id: 4, title: "Введення в функції", path: '/introducing-fn'  },
-        { id: 5, title: "Замикання, коллбеки, функцій вищого порядку та IIFE", path: '/circuits-callbacks-fn'  },
-        { id: 6, title: "Стрілкові функції", path: '/arrow-fn'  },
-        { id: 7, title: "Рекурсія та вступ до теми функцій констркуторів", path: '/recursion'  },
-        { id: 8, title: "Array та Object", path: '/array' },
-        { id: 9, title: "DOM та Events", path: '/dom-events' },
-        { id: 10, title: "Об'єкт Event, Фази подій, Делегування. BOM", path: '/obj-events'  },
-        { id: 11, title: "Форми та регулярні вирази", path: '/forms'  },
-        { id: 12, title: "Coding Best Practices та чистий код", path: '/best-practice'  },
-        { id: 13, title: "WebStorage", path: '/web-storage'  },
-        { id: 14, title: "OOП JavaScript", path: '/oop-js'  },
-        { id: 15, title: "Class", path: '/class' },
-        { id: 16, title: "Ведення в Асинхронність, Event Loop та Promise", path: '/promise'  },
-        { id: 17, title: "Ajax та async…await", path: '/ajax'  },
-        { id: 18, title: "jQuery", path: '/jquery'  },
-        { id: 19, title: "TypeScript", path: '/typescript'  },
-    ];
-
     return (
         <div className="page-container">
             <h1>Перелік тем</h1>
--- src/components/route/router.jsx
+++ src/components/route/router.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import Layout from '../index.jsx';
-import MainPage from '../../pages/mainpage.jsx';
-import ContactsPage from '../Info/Contact/Contact.jsx';
-import AboutPage from '../Info/AboutUs/AboutUs.jsx';
-import Content from '../Content/Content.jsx';
-import NineteenthTheme from "../../pages/subPages/TypeScript/NineteenthTheme.jsx";
-import EighteenthTheme from "../../pages/subPages/JQuery/EighteenthTheme.jsx";
-import SeventeenthTheme from "../../pages/subPages/AjaxAndAsyncAwait/SeventeenthTheme.jsx";
-import SixteenthTheme from "../../pages/subPages/IntroductionToAsynchronyEventLoopAndPromise/SixteenthTheme.jsx";
-import FifteenthTheme from "../../pages/subPages/Class/FifteenthTheme.jsx";
-import FourteenthTheme from "../../pages/subPages/OOPJs/FourteenthTheme.jsx";
-import ThirteenthTheme from "../../pages/subPages/WebStorage/ThirteenthTheme.jsx";
-import TwelfthTheme from "../../pages/subPages/CodingBestPracticesForCleanCode/TwelfthTheme.jsx";
-import EleventhTheme from "../../pages/subPages/FormsAndRegularExpressions/EleventhTheme.jsx";
-import TenthTheme from "../../pages/subPages/ObjectEventPhasesDelegationBOM/TenthTheme.jsx";
-import NinthTheme from "../../pages/subPages/DOMEvent/NinthTheme.jsx";
-import EightTheme from "../../pages/subPages/ArrayObject/EightTheme.jsx";
-import SeventhTheme from "../../pages/subPages/RecursionAndAnIntroductionToTheTopicOfConstructorFunctions/SeventhTheme.jsx";
-import SixthTheme from "../../pages/subPages/ArrowFunctions/SixthTheme.jsx";
-import FifthTheme from "../../pages/subPages/CircuitsCallbacksFunctionsOfTheHighestOrderAndIIFE/FifthTheme.jsx";
-import FourthTheme from "../../pages/subPages/Function/FourthTheme.jsx";
-import ThirdTheme from "../../pages/subPages/Cycles/ThirdTheme.jsx";
-import SecondTheme from "../../pages/subPages/Operators/SecondTheme.jsx";
-import FirstTheme from "../../pages/subPages/VariousTypesOfData/FirstTheme.jsx";
+import {
+    Layout,
+    MainPage,
+    Content,
+    ContactsPage,
+    AboutPage,
+    NineteenthTheme,
+    EighteenthTheme,
+    SeventeenthTheme,
+    SixteenthTheme,
+    FifteenthTheme,
+    FourteenthTheme,
+    ThirteenthTheme,
+    TwelfthTheme,
+    EleventhTheme,
+    TenthTheme,
+    NinthTheme,
+    EightTheme,
+    SeventhTheme,
+    SixthTheme,
+    FifthTheme,
+    FourthTheme,
+    ThirdTheme,
+    SecondTheme,
+    FirstTheme
+} from './index.js'
 
 
 const routes = [
